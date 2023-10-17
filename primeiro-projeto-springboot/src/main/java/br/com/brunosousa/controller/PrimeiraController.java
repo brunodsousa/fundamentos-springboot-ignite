0ff202b6e3b8a23d5257b4e620dd78f021fe1461
Bruno Sousa <80365075+brunodsousa@users.noreply.github.com>
2023-10-16 23:28:45 -0300

    Path Params

--- primeiro-projeto-springboot/src/main/java/br/com/brunosousa/controller/PrimeiraController.java
+++ primeiro-projeto-springboot/src/main/java/br/com/brunosousa/controller/PrimeiraController.java
@@ -1,16 +1,33 @@
 package br.com.brunosousa.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "Parabéns! Você criou sua primeira rota.";
+    @GetMapping("/primeiroMetodo/{id}")
+    // /primeiraController/primeiroMetodo/189
+    public String primeiroMetodo(@PathVariable String id) {
+        return "O parâmetro é " + id;
     }
 
+    @GetMapping("/metodoComQueryParams")
+    // /primeiraController/metodoComQueryParams?id=189
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "O parâmetro com metodoComQueryParams é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    // /primeiraController/metodoComQueryParams?id=189&nome=Bruno
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+        // O parâmetro com metodoComQueryParams2 é [id=189, nome=Bruno]
+        return "O parâmetro com metodoComQueryParams2 é " + allParams.entrySet();
+    }
 }
